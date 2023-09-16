f829b5bf7d6ecf59a0fcc4a271837815d7a86f78
Rosen Todorov <rosen.vt55@gmail.com>
2023-09-16 12:00:19 +0300

    Implemented home view

--- Cat-Shelter/index.js
+++ Cat-Shelter/index.js
@@ -1,11 +1,29 @@
 const http = require('http');
+const fs = require('fs/promises');
 const url = require('url');
+const { log } = require('console');
 const port = 5050;
 
-const server = http.createServer((req,res)=>{
-    console.log(url.parse(req['url']).pathname);
+const server = http.createServer(async(req,res)=>{
+    const {url} = req;
 
-    res.write('Hello from server')
+    if(url === '/'){
+        res.writeHead(200,{
+            'Content-Type': 'text/html',
+        });
+
+        const homeTemplate = await fs.readFile('./views/home/index.html', 'utf8');
+        res.write(homeTemplate);
+    }
+    else if( url === '/content/styles/site.css'){
+        res.writeHead(200,{
+            'Content-Type': 'text/css',
+        });
+
+        const siteCss = await fs.readFile('./content/styles/site.css', 'utf8');
+        res.write(siteCss);
+    }
+
     res.end();
 });
 
