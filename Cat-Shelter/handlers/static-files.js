6e3d52af83e08d30c3845e57a511b33b15654643
Rosen Todorov <rosen.vt55@gmail.com>
2023-09-17 12:56:40 +0300

    added statics files handler

---
+++ Cat-Shelter/handlers/static-files.js
@@ -0,0 +1,45 @@
+const url = require('url');
+const fs = require('fs/promises');
+const path = require('path');
+
+
+function getContentType(url){
+    if(url.endsWith('css')){
+        return 'text/css';
+    }else if(url.endsWith('html')){
+        return 'text/html';
+    }else if(url.endsWith('js')){
+        return 'text/javascript';
+    }else{
+        return 'tesxt/plain'
+    }
+}
+
+module.exports = async (req,res) => {
+    const pathName = url.parse(req.url).pathname;
+    console.log(pathName);
+    if(pathName.startsWith('/content') && req.method === 'GET'){
+
+        const filePath = `.${pathName}`;
+        console.log('here');
+        const data = await fs.readFile(filePath, 'utf8');
+
+        if(!data){
+            res.writeHead(400, {
+                "Content-Type": "text/plain"
+            })
+            res.write("400 Not Found");
+            res.end();
+            return;
+        }
+
+        res.writeHead(200, {
+            "Content-Type": getContentType(pathName),
+        })
+        res.write(data);
+        res.end();
+
+    }else{
+        return true;
+    }
+}
