d7f8d50916a84f2dbd59a5c5b912a1f0729067d2
Rosen Todorov <rosen.vt55@gmail.com>
2023-09-17 11:05:15 +0300

    Added add-breed functionality

--- Cat-Shelter/index.js
+++ Cat-Shelter/index.js
@@ -1,30 +1,70 @@
-const http = require('http');
-const fs = require('fs/promises');
-const url = require('url');
-const { log } = require('console');
+const http = require("http");
+const fs = require("fs/promises");
+const url = require("url");
+const qs = require("querystring");
+const { error, log } = require("console");
 const port = 5050;
 
-const server = http.createServer(async(req,res)=>{
-    const {url} = req;
+const server = http.createServer(async (req, res) => {
+  const { url } = req;
 
-    if(url === '/'){
-        res.writeHead(200,{
-            'Content-Type': 'text/html',
-        });
+  if (url === "/") {
+    res.writeHead(200, {
+      "Content-Type": "text/html",
+    });
 
-        const homeTemplate = await fs.readFile('./views/home/index.html', 'utf8');
-        res.write(homeTemplate);
-    }
-    else if( url === '/content/styles/site.css'){
-        res.writeHead(200,{
-            'Content-Type': 'text/css',
-        });
+    const homeTemplate = await fs.readFile("./views/home/index.html", "utf8");
+    res.write(homeTemplate);
+  } else if (url === "/cats/add-breed" && req.method === "GET") {
+    res.writeHead(200, {
+      "Content-Type": "text/html",
+    });
 
-        const siteCss = await fs.readFile('./content/styles/site.css', 'utf8');
-        res.write(siteCss);
-    }
+    const addBreedTemplate = await fs.readFile("./views/addBreed.html", "utf8");
+    res.write(addBreedTemplate);
+  } else if (url === "/cats/add-breed" && req.method === "POST") {
+    let formData = "";
 
+    req.on("data", (data) => {
+      formData += data;
+    });
+
+    req.on("end", async () => {
+      let body = qs.parse(formData);
+      console.log(body);
+
+      const data = await fs.readFile("./data/breeds.json", "utf8");
+
+      if (!data) {
+        throw error("Unexpected error");
+      }
+
+      let breeds = JSON.parse(data);
+      breeds.push(body.breed);
+      console.log(breeds);
+      let result = JSON.stringify(breeds);
+
+      await fs.writeFile("./data/breeds.json", result, "utf-8");
+    });
+    res.writeHead(301, { location: "/" });
     res.end();
+  } else if (url === "/cats/add-cat") {
+    res.writeHead(200, {
+      "Content-Type": "text/html",
+    });
+
+    const addBreedTemplate = await fs.readFile("./views/addCat.html", "utf8");
+    res.write(addBreedTemplate);
+  } else if (url === "/content/styles/site.css") {
+    res.writeHead(200, {
+      "Content-Type": "text/css",
+    });
+
+    const siteCss = await fs.readFile("./content/styles/site.css", "utf8");
+    res.write(siteCss);
+  }
+
+  res.end();
 });
 
-server.listen(port, ()=> console.log(`Server is listening on port ${port}`));
+server.listen(port, () => console.log(`Server is listening on port ${port}`));
