5be59821712f0f75eb854d6e43d27071d5e120a6
Rosen Todorov <rosen.vt55@gmail.com>
2023-09-19 19:10:13 +0300

    Implemented cat new home logic

--- Cat-Shelter/handlers/cat.js
+++ Cat-Shelter/handlers/cat.js
@@ -125,22 +125,55 @@ module.exports = async (req, res) => {
     req.on("data", (data) => {
       formData += data;
     });
-    req.on("end", async() => {
-      const catsPath = path.normalize(path.join(__dirname, "../data/cats.json"));
+    req.on("end", async () => {
+      const catsPath = path.normalize(
+        path.join(__dirname, "../data/cats.json")
+      );
       const body = qs.decode(formData);
 
       const catId = pathName.slice(pathName.lastIndexOf("/") + 1);
-      console.log(catId);
+
       cats[catId - 1].name = body.name;
       cats[catId - 1].imageUrl = body.imageUrl;
       cats[catId - 1].description = body.description;
       cats[catId - 1].breed = body.breed;
-      // console.log(cats);
+
       const result = JSON.stringify(cats);
       await fs.writeFile(catsPath, result, "utf-8");
 
-      res.writeHead(301, {location: '/'});
+      res.writeHead(301, { location: "/" });
       res.end();
     });
+  } else if (pathName.includes("/cats-new-home") && req.method === "GET") {
+    const shelterCatTemplatePath = path.normalize(
+      path.join(__dirname, "../views/catShelter.html")
+    );
+
+    const catId = pathName.slice(pathName.lastIndexOf("/") + 1);
+    const cat = cats[catId - 1];
+    const shelterCatTemplate = await fs.readFile(
+      shelterCatTemplatePath,
+      "utf-8"
+    );
+    const modifiedShelterTemplate = replaceData(shelterCatTemplate, cat);
+
+    res.writeHead(200, {
+      "Content-Type": "text/html",
+    });
+    res.write(modifiedShelterTemplate);
+    res.end();
+  } else if (pathName.includes("/cats-new-home") && req.method === "POST") {
+    const catsPath = path.normalize(path.join(__dirname, "../data/cats.json"));
+    const catId = pathName.slice(pathName.lastIndexOf("/") + 1);
+    console.log(cats);
+    const newCats = cats.filter((cat) => cat.id != catId);
+    //console.log(newCats);
+
+    const result = JSON.stringify(newCats);
+
+    await fs.writeFile(catsPath, result, "utf-8");
+
+    res.writeHead(301, { location: "/" });
+    res.end();
   }
 };
