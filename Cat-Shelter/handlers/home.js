0ee41a020d7ccc7cdff17fbd0fadadf3bfd9cb4e
Rosen Todorov <rosen.vt55@gmail.com>
2023-09-19 13:01:12 +0300

    Implemented dynamic cats render in home page

--- Cat-Shelter/handlers/home.js
+++ Cat-Shelter/handlers/home.js
@@ -3,13 +3,23 @@ const fs = require('fs/promises');
 const path = require('path');
 const cats = require('../data/cats');
 
+const replaceData = (html,data) =>{
+    return Object.keys(data).reduce((result, prop) =>{
+        result = result.replaceAll(`{{${prop}}}`, data[prop]);
+        return result;
+    },html)
+}
 
 module.exports = async (req,res) =>{
     const pathName = url.parse(req.url).pathname;
 
     if(pathName === '/' && req.method === 'GET'){
-        const filePath = path.normalize(path.join(__dirname, '../views/home/index.html'))
-        const data = await fs.readFile(filePath, 'utf8');
+
+        const homeTemplatePath = path.normalize(path.join(__dirname, '../views/home/index.html'))
+        const catTemplatePath = path.normalize(path.join(__dirname, '../views/home/catTemplate.html'))
+        const catDataPath = path.normalize(path.join(__dirname, '../data/cats.json'))
+
+        const data = await fs.readFile(homeTemplatePath, 'utf8');
         if(!data){
             res.writeHead(400, {
                 "Content-Type": "text/plain"
@@ -19,12 +29,20 @@ module.exports = async (req,res) =>{
             return;
         }
 
+        const catTamplate = await fs.readFile(catTemplatePath, 'utf8');
+        const catsData = await fs.readFile(catDataPath, 'utf8');
+        const cats = JSON.parse(catsData);
+        console.log(cats);
+
+        const catsHtml = cats.map(cat => replaceData(catTamplate, cat));
+        const homeTemplate = replaceData(data, {cats: catsHtml});
+        console.log(catsHtml);
+
         res.writeHead(200, {
             "Content-Type": "text/html"
         })
-        res.write(data);
+        res.write(homeTemplate);
         res.end();
-        console.log('end')
 
     }else{
         return true;
