b708942c8b5ec53b1f86e9aee7ee471984d3fc34
Rosen Todorov <rosen.vt55@gmail.com>
2023-10-07 16:37:09 +0300

    Created controller and service for user logic

--- Cubicle/src/router.js
+++ Cubicle/src/router.js
@@ -2,10 +2,12 @@ const router = require('express').Router();
 const homeController = require('./controllers/homeController');
 const cubeController = require('./controllers/cubeController');
 const accesoryController = require('./controllers/accesoryController');
+const userController = require('./controllers/userController');
 
 router.use(homeController);
 router.use("/cubes", cubeController);
 router.use('/accesory', accesoryController);
+router.use('/users', userController);
 
 router.get('*', (req,res) =>{
     res.redirect("/404");
