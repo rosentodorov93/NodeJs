87a74592730129b1952fd46a096a601e8a9c2775
Rosen Todorov <rosen.vt55@gmail.com>
2023-10-09 16:12:50 +0300

    Moved cookieParser config to expressConfig

--- Cubicle/src/config/expressConfig.js
+++ Cubicle/src/config/expressConfig.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const path = require('path');
+const cookieParser = require('cookie-parser')
 
 
 const expressConfig = (app) =>{
     app.use(express.static(path.resolve(__dirname, '../static')));
     app.use(express.urlencoded({extended: false}));
+    app.use(cookieParser());
 }
 
 module.exports = expressConfig;
