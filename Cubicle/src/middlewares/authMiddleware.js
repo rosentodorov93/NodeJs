502cb21a160326fcf71bf6f69daf0c416228b11c
Rosen Todorov <rosen.vt55@gmail.com>
2023-10-10 15:18:16 +0300

    Added authMiddleware

---
+++ Cubicle/src/middlewares/authMiddleware.js
@@ -0,0 +1,23 @@
+const jwt = require('../lib/jwt');
+const {SECRET} = require('../constants');
+
+exports.authUser = async(req, res, next) =>{
+    const token = req.cookies["token"];
+    if(token){
+        try{
+            const decodedToken = await jwt.verify(token, SECRET);
+            req.user = decodedToken;
+            res.locals.user = decodedToken;
+            res.locals.isAuthenticated = true;
+            next();
+        }catch(error){
+            console.log({error});
+            res.clearCookie("token");
+            res.redirect('/users/login');
+        }        
+    }else{
+        res
+        next();
+    }
+
+}
