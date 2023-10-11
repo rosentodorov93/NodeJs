ecbdd012a9c233956988c2ceb03b9d535ef565fa
Rosen Todorov <rosen.vt55@gmail.com>
2023-10-11 13:30:17 +0300

    Added logout endpoint

--- Cubicle/src/controllers/userController.js
+++ Cubicle/src/controllers/userController.js
@@ -31,4 +31,10 @@ router.post('/register', async (req, res) =>{
     res.redirect('/users/login')
 })
 
+router.get('/logout', async (req, res) =>{
+    res.clearCookie("token");
+    res.redirect('/')
+})
+
+
 module.exports = router;
