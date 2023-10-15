22ca7d61fc5af0590e66e64da3e8e8bd78a0cda8
Rosen Todorov <rosen.vt55@gmail.com>
2023-10-15 10:43:00 +0300

    Added validation in user class

--- Cubicle/src/models/User.js
+++ Cubicle/src/models/User.js
@@ -1,8 +1,32 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
-    username: String,
-    password: String,
+    username: {
+        type: String,
+        reqired: [true, 'Username is required'],
+        minLength: [3, "Username must be atleast 3 characters long"],
+    },
+    password: {
+        type: String,
+        reqired: [true, 'Password is required'],
+        minLength: [3, "Password must be atleast 3 characters long"],
+        maxLength: [8, "Password must can not be more than 8 character long"],
+    }
+})
+
+userSchema.path('username').validate(function(username){
+    const user = mongoose.model("User").findOne({username});
+    return !!user;
+})
+userSchema.virtual("repeatPassword").set(function(value){
+    if(value !== this.password){
+        this.invalidate('password', 'must match with reapeat password')
+    }
+})
+userSchema.pre('save', async function(){
+    const hash = await bcrypt.hash(this.password, 10);
+    this.password = hash;
 })
 
 const User = mongoose.model("User", userSchema);
